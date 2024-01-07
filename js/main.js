2da28c3225dd88eb5fba05c4f81b8908a7cf3982
luizth25 <luizftp25@gmail.com>
2024-01-07 17:41:04 -0300

    adding load more button and style for cards based on type pokemon

--- js/main.js
+++ js/main.js
@@ -1,36 +1,39 @@
-// API = https://pokeapi.co/api/v2/pokemon
-// API = https://pokeapi.co/api/v2/pokemon?offset=0&limit=10
-
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+const loadMoreButton = document.getElementById("loadMore");
+const pokemonList = document.getElementById("pokemon-list");
+let offset = 0;
+const limit = 5;
 
 const convertPokemonToHtml = (pokemon) => {
-  return `<li class="pokemon">
-  <span class="number">#001</span>
+  console.log(pokemon);
+
+  return `<li class='pokemon ${pokemon.type}'>
+  <span class="number">##${pokemon.id}</span>
   <span class="name">${pokemon.name}</span>
   <div class="detail">
     <ol class="types">
-      <li class="type">grass</li>
-      <li class="type">poison</li>
+   ${pokemon.types
+     .map((type) => `<li class="type ${type}">${type}</li>`)
+     .join(" ")}
     </ol>
     <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      src=${pokemon.image}
       alt=${pokemon.name}
     />
   </div>
 </li>`;
 };
 
-const pokemonList = document.getElementById("pokemon-list");
+const loadMorePokemonItems = (offset, limit) => {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newList = pokemons.map((pokemon) => convertPokemonToHtml(pokemon));
+    const newHtml = newList.join(" ");
+    pokemonList.innerHTML += newHtml;
+  });
+};
 
-fetch(url)
-  .then((response) => response.json())
-  .then((responseBody) => responseBody.results)
-  .then((pokemons) => {
-    for (let i = 0; i < pokemons.length; i++) {
-      const pokemon = pokemons[i];
-      pokemonList.innerHTML += convertPokemonToHtml(pokemon);
-    }
-  })
-  .catch((e) => console.log(e));
+loadMorePokemonItems(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  loadMorePokemonItems(offset, limit);
+});
